22c6f9634eabdea112024d03c9dc0545dc43cf26
Семён Субочев <s.subochev@sz.ld>
2024-02-15 12:39:20 +0300

    кастом хук для карт в крзине = totalPrice

---
+++ src/components/hooks/useCard.js
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import { AppContext } from '../../App';
+
+export const useCard = () => {
+	const { cartItems, setCartItems } = useContext(AppContext);
+	const totalPrice = cartItems.reduce((acc, obj) => obj.price + acc, 0);
+	return { cartItems, setCartItems, totalPrice };
+};
